37c9ed8c801dee6a9748d068e137ed6781e6b5f2
Flipmemax <maxphoogland@gmail.com>
2021-05-21 12:59:38 +0200

    setup relations and model skeletons

---
+++ models/product.js
@@ -0,0 +1,28 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      product.belongsTo(models.category);
+      // define association here
+    }
+  }
+  product.init(
+    {
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      price: DataTypes.INTEGER,
+      image: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "product",
+    }
+  );
+  return product;
+};
